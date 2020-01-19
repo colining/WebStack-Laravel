2db43a1a18e6a5046ced5f0abadb13d3543d8be9
colining <18810782954@163.com>
2020-01-19 21:12:16 +0800

    add wufu page

---
+++ public/wufu.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="zh">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="viggo">
+    <meta name="referrer" content="no-referrer" />
+    <title>WebStack</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="无火余灰| 导航站 汇集知乎智慧，收集国内外新奇网站，壁纸网站，实用网站，摸鱼网站，定期更新最有趣的精选网站, wuhuoyuhui.cn">
+    <link rel="shortcut icon" href="http://wuhuoyuhui.cn/img/favicon.png">
+    <link rel="preload" href="http://fonts.googleapis.com/css?family=Arimo:400,700,400italic" as="font">
+    <link rel="stylesheet" href="/css/app.css?id=08f4ad15e83f6df13efe">
+    <script src="/js/app.js?id=f9f0907779c993f5cbc4"></script>
+    <link rel="stylesheet" href="css/search.css">
+    <link rel="stylesheet" href="css/fu.css">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153987603-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-153987603-1');
+    </script>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- / FB Open Graph -->
+    <meta property="og:type" content="article">
+    <meta property="og:url" content="http://www.webstack.cc/">
+    <meta property="og:title" content="无火余灰| 导航站 汇集知乎智慧，收集国内外新奇网站，壁纸网站，实用网站，摸鱼网站，定期更新最有趣的精选网站, wuhuoyuhui.cn">
+    <meta property="og:description" content="无火余灰,网址导航,知乎网址导航,灵感新奇网站,实用工具,新媒体网址导航,定期更新最有趣的精选网站, wuhuoyuhui.cn">
+    <meta property="og:image" content="http://wuhuoyuhui.cn/img/webstack_banner_cn.png">
+    <meta property="og:site_name" content="无火余灰| 导航站 汇集知乎智慧，收集国内外新奇网站,创意导航,壁纸网站,实用网站,摸鱼网站,定期更新最有趣的精选网站, wuhuoyuhui.cn">
+    <!-- / Twitter Cards -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="无火余灰| 导航站 汇集知乎智慧，收集国内外新奇网站，壁纸网站，实用网站，摸鱼网站，定期更新最有趣的精选网站, wuhuoyuhui.cn">
+    <meta name="twitter:description" content="无火余灰,网址导航,知乎网址导航,灵感新奇网站,实用工具,新媒体网址导航,定期更新最有趣的精选网站, wuhuoyuhui.cn">
+    <meta property="og:image" content="http://wuhuoyuhui.cn/img/webstack_banner_cn.png">
+</head>
+<body class="page-body">
+<!-- skin-white -->
+<div class="page-container">
+
+    <div class="sidebar-menu toggle-others fixed">
+        <div class="sidebar-menu-inner">
+            <header class="logo-env">
+                <!-- logo -->
+                <div class="logo">
+                    <a href="/" class="logo-expanded">
+                        <img src="http://wuhuoyuhui.cn/img/logo@2x.png" width="100%" alt=""/>
+                    </a>
+                    <a href="/" class="logo-collapsed">
+                        <img src="http://wuhuoyuhui.cn/img/logo-collapsed@2x.png" width="40" alt=""/>
+                    </a>
+                </div>
+                <div class="mobile-menu-toggle visible-xs">
+                    <a href="#" data-toggle="user-info-menu">
+                        <i class="linecons-cog"></i>
+                    </a>
+                    <a href="#" data-toggle="mobile-menu">
+                        <i class="fa-bars"></i>
+                    </a>
+                </div>
+            </header>
+            <ul id="main-menu" class="main-menu">
+                <li>
+                    <a href="/">
+                        <i class="fa fa-fw fa-star-o"></i>
+                        <span class="title">主页</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="main-content">
+        <nav class="navbar user-info-navbar" role="navigation">
+            <!-- User Info, Notifications and Menu Bar -->
+            <!-- Left links for user info navbar -->
+            <ul class="user-info-menu left-links list-inline list-unstyled">
+                <li class="hidden-sm hidden-xs">
+                    <a href="#" data-toggle="sidebar">
+                        <i class="fa-bars"></i>
+                    </a>
+                </li>
+                <li class="weather">
+                    <div id="he-plugin-simple"></div>
+                    <script type="text/javascript" src="/js/weather.js"></script>
+                    <script src="https://widget.heweather.net/simple/static/js/he-simple-common.js?v=1.1"></script>
+                </li>
+            </ul>
+        </nav>
+
+        <script type="text/javascript" src="/js/search.js"></script>
+
+
+        <div id="root">
+            <?php
+            for ($i=1; $i<=57; $i++)
+            {
+                echo "
+                     <div class=\"item\">
+                        <img class=\"itemImg\" src=\"./img/fu/fu-{$i}.jpg\" /> 
+                    </div>";
+            }
+            ?>
+        </div>
+
+        <br>
+
+        <!-- Main Footer -->
+        <!-- Choose between footer styles: "footer-type-1" or "footer-type-2" -->
+        <!-- Add class "sticky" to  always stick the footer to the end of page (if page contents is small) -->
+        <!-- Or class "fixed" to  always fix the footer to the end of page -->
+        <footer class="main-footer sticky footer-type-1">
+            <div class="footer-inner">
+                <!-- Add your copyright text here -->
+                <div class="footer-text">
+                    Copyright &copy; 2019
+                    <a href="https://github.com/colining" target="_blank">
+                        <strong>Colin</strong>
+                    </a> All Rights Reserved.
+
+                    Theme by
+                    <a href="https://github.com/hui-ho/WebStack-Laravel">
+                        <strong>WebStack</strong>
+                    </a>
+                    <!--  - Purchase for only <strong>23$</strong> -->
+                </div>
+                <!-- Go to Top Link, just add rel="go-top" to any link to add this functionality -->
+                <div class="go-up">
+                    <a href="#" rel="go-top">
+                        <i class="fa-angle-up"></i>
+                    </a>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+<!-- 锚点平滑移动 -->
+<script type="text/javascript">
+    $(document).ready(function() {
+        $(document).on('click', '.has-sub', function(){
+            var _this = $(this)
+            if(!$(this).hasClass('expanded')) {
+                setTimeout(function(){
+                    _this.find('ul').attr("style","")
+                }, 300);
+
+            } else {
+                $('.has-sub ul').each(function(id,ele){
+                    var _that = $(this)
+                    if(_this.find('ul')[0] != ele) {
+                        setTimeout(function(){
+                            _that.attr("style","")
+                        }, 300);
+                    }
+                })
+            }
+        })
+        $('.user-info-menu .hidden-sm').click(function(){
+            if($('.sidebar-menu').hasClass('collapsed')) {
+                $('.has-sub.expanded > ul').attr("style","")
+            } else {
+                $('.has-sub.expanded > ul').show()
+            }
+        })
+        $("#main-menu li ul li").click(function() {
+            $(this).siblings('li').removeClass('active'); // 删除其他兄弟元素的样式
+            $(this).addClass('active'); // 添加当前元素的样式
+        });
+        $("a.smooth").click(function(ev) {
+            ev.preventDefault();
+
+            public_vars.$mainMenu.add(public_vars.$sidebarProfile).toggleClass('mobile-is-visible');
+            ps_destroy();
+            $("html, body").animate({
+                scrollTop: $($(this).attr("href")).offset().top - 30
+            }, {
+                duration: 500,
+                easing: "swing"
+            });
+        });
+        return false;
+    });
+
+    var href = "";
+    var pos = 0;
+    $("a.smooth").click(function(e) {
+        $("#main-menu li").each(function() {
+            $(this).removeClass("active");
+        });
+        $(this).parent("li").addClass("active");
+        e.preventDefault();
+        href = $(this).attr("href");
+        pos = $(href).position().top - 30;
+    });
+</script>
+</body>
+
+</html>
+
